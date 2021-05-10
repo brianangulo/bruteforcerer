d55d56bd94268c55eb7c23847ad3bd33223c3ec3
Brian Angulo <brianangulo96@gmail.com>
2021-05-10 00:22:36 -0400

    Home & forms

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setPassword } from "../redux/slicer";
-import { TextField } from "@material-ui/core";
-
+import { TextField, Button } from "@material-ui/core";
 
 function Home() {
-    const dispatch = useDispatch();
-    const password = useSelector((state) => state.slicer.password)
-    
-    return (
+  const dispatch = useDispatch();
+  const password = useSelector((state) => state.slicer.password);
+
+  const handleChange = (value) => {
+    dispatch(setPassword(value.target.value))
+  }
+
+  const handleSubmit = () => {
+      console.log(password);
+      dispatch(setPassword(""));
+  }
+
+  return (
+    <div>
       <div>
-        <form noValidate autoComplete="off">
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </form>
+        <p>Lets see how secure is your password?</p>
       </div>
-    );
+      <form noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Your Password"
+          variant="outlined"
+          onChange={handleChange}
+          value={password}
+        />
+      </form>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,7 @@ import Home from "./Home";
 function Main() {
     
     return (
-      <div>
-        <p>This is for you right here!!</p>
         <Home />
-      </div>
     );
 }
 
