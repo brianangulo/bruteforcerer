2c1b2f71896f9276cd980d67de083bbb0323336b
Brian Angulo <brianangulo96@gmail.com>
2021-05-10 20:00:45 -0400

    Updating redux slicer as needed

--- src/redux/slicer.js
+++ src/redux/slicer.js
@@ -4,14 +4,26 @@ export const slice = createSlice({
     name: "slicer",
     initialState: {
         password: "",
+        timer: "",
+        enteredPass: "",
+        toggleReveal: false,
     },
     reducers: {
         setPassword: (state, action) => {
             state.password = action.payload
+        },
+        setTimer: (state, action) => {
+            state.timer = action.payload
+        },
+        setEnteredPas: (state, action) => {
+            state.enteredPass = action.payload
+        },
+        setToggleReveal: (state, action) => {
+            state.toggleReveal = action.payload
         }
     }
 })
 
-export const { setPassword } = slice.actions
+export const { setPassword, setTimer, setEnteredPas, setToggleReveal } = slice.actions
 
 export default slice.reducer
