f3dc6cfe32d625b21232299d133b2d772f2d58d7
Brian Angulo <brianangulo96@gmail.com>
2021-05-10 20:00:15 -0400

    Bruteforcing set up

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,62 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setPassword } from "../redux/slicer";
+import {
+  setPassword,
+  setTimer,
+  setEnteredPas,
+  setToggleReveal,
+} from "../redux/slicer";
 import { TextField, Button } from "@material-ui/core";
-import { bruteforce } from "bruteforcejs";
+import { chars } from "../shared/chars";
+import RevealText from "./RevealText";
 
-function Home() {
+const bruteforce = require("bruteforcejs");
+
+function Home(props) {
+  //dispatcher
   const dispatch = useDispatch();
+  //state variables
   const password = useSelector((state) => state.slicer.password);
+  const timer = useSelector((state) => state.slicer.timer);
+  const enteredPass = useSelector((state) => state.slicer.enteredPass);
+  const toggleReveal = useSelector((state) => state.slicer.toggleReveal);
 
+  //handles change on the form
   const handleChange = (value) => {
-    dispatch(setPassword(value.target.value))
-  }
+    dispatch(setPassword(value.target.value));
+  };
 
-  const handleBruting = () => {
-    bruteforce("ABCabc", (result) => {
-      console.log(result); // Display current bruteforce string
-      if (result === "abBac") {
-        // If current bruteforce string is right
-        return true; // Return true => finish bruteforce
-      }
-    })
-  }
-
+  //Keeping it all together for better perfomance bruteforcing algorithm on submit
   const handleSubmit = () => {
-      console.log(password);
-      handleBruting();
-      dispatch(setPassword(""));
-  }
+    console.log(password);
+    //Controling toggle for reveal text
+    dispatch(setToggleReveal(true));
+    //Setting timer
+    const bfInit = performance.now();
+    bruteforce(
+      chars,
+      (result) => {
+        if (result === password) {
+          const bfEnd = performance.now();
+          dispatch(setEnteredPas(result));
+          dispatch(setTimer((bfEnd - bfInit).toFixed(2)));
+          console.log(
+            `The last result is: ${result} and time is ${(
+              bfEnd - bfInit
+            ).toFixed(2)}ms`
+          );
+          return true;
+        }
+      },
+      4
+    );
+    dispatch(setPassword(""));
+  };
 
   return (
     <div>
       <div>
-        <p>Lets see how secure is your password?</p>
+        <h3>Lets see how secure is your password?</h3>
       </div>
       <form noValidate autoComplete="off">
         <TextField
@@ -43,13 +68,12 @@ function Home() {
         />
       </form>
       <div>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-        >
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
           Submit
         </Button>
+      </div>
+      <div>
+        <RevealText timer={timer} enteredPass={enteredPass} toggleReveal={toggleReveal} />
       </div>
     </div>
   );
