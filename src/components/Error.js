5e4bc3c36f982fee1222a3e08836ba534a728765
Brian Angulo <brianangulo96@gmail.com>
2021-05-16 19:17:38 -0400

    Error center bug fix

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Error(props) {
     
     return (
-        <p hidden={props.isHidden} className="row text-center" style={{color: "red", fontWeight: "bold"}} >For the sake of your time we can only test 4 alpha-num characters or less!</p>
+        <p hidden={props.isHidden} className="row text-center justify-content-center" style={{color: "red", fontWeight: "bold"}} >For the sake of your time we can only test 4 alpha-num characters or less!</p>
     );
 }
 
