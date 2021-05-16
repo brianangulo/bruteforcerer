bb2ff49c02d2c94a74810b49b94aeaff61a36159
Brian Angulo <brianangulo96@gmail.com>
2021-05-16 18:52:16 -0400

    Moving contact feature to new branch

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 
 function NavBar() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
         <div className="container" >
-          <Navbar.Brand className="ml-auto" href="#home">Brute Force-It</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Brand className="ml-auto">Brute Force-It</Navbar.Brand>
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav>
-              <Nav.Link href="#features">Contact</Nav.Link>
-            </Nav>
           </Navbar.Collapse>
         </div>
       </Navbar>
